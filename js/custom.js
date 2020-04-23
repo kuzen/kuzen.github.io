445636b518f5f12dd9b539992205e0eba017aded
sj Chen <jldjz97@outlook.com>
2020-04-23 14:29:17 +0800

    Site updated: 2020-04-23 14:29:12

--- js/custom.js
+++ js/custom.js
@@ -1,17 +1,11 @@
 $(document).ready(() => {
-    $('#back-to-top').on('click', () => {
-        $('body, html').animate({ scrollTop: 0 }, 400);
-    });
 	
 	$('.container').eq(1).click(function(event){
 		event.stopPropagation();
-		return false;
 	});   
 	
     $('.section').on('click', () => {
 		var timestamp = Date.parse(new  Date());
         $('.is-2-column').css('background-image', 'url(https://api.ixiaowai.cn/api/api.php?timestamp='+timestamp+')');
     });
-
-
 });
